c91a30c0accae0890db8c61c7465a6a25aa7be0d
Peerasait Prachaseree (Jeffrey) <89213088+pprachas@users.noreply.github.com>
2022-01-27 14:08:07 -0500

    Update subdataset2_img.py

--- Domain/subdataset2_domain/subdataset2_img.py
+++ Domain/subdataset2_domain/subdataset2_img.py
@@ -10,8 +10,8 @@ w = 100 # wdith of column
 r = int(0.2*100)
 
 #----------------------------File Path-----------------------------#
-f_x = 'intersect_donut_bulk/img/subdataset2_geo/x/x'
-f_y = 'intersect_donut_bulk/img/subdataset2_geo/y/y'
+f_x = 'intersect_donut_bulk/img/subdataset2_geometry/x/x'
+f_y = 'intersect_donut_bulk/img/subdataset2_geometry/y/y'
 
 def img_gen(L,w,f_x,f_y,r,ii):
   L = L
